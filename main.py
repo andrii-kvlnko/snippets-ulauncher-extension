630b4217d88e28498f5fd998140845c8e86f8c6d
user <user@example.com>
2023-08-06 23:10:53 +0300

    Show list of json items

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+import json
 
+class JSONRepository:
+    def __init__(self, path):
+        self.path = path;
+
+    def get_items():
+        file = open(filename, 'r')
+        content = file.read()
+        document = json.loads(content);
+
+        return document["items"]
+
+    __path
 
 class DemoExtension(Extension):
 
@@ -16,12 +29,18 @@ class DemoExtension(Extension):
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
-        items = []
-        for i in range(5):
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name='Item %s' % i,
-                                             description='Item description %s' % i,
-                                             on_enter=HideWindowAction()))
+        path = extension.preferences['source']
+        repository = JSONRepository(path)
+        repository_items = repository.get_items()
+
+        result_items = []
+
+        for repository_item in repository_items:
+            result_item = ExtensionResultItem(icon='',
+                                          name='Item %s' % repository_item,
+                                          description='Item description %s' % repository_item,
+                                          on_enter=HideWindowAction())
+            result_items.append(result_item)
 
         return RenderResultListAction(items)
 
